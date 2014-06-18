f7a0a09b0faad2e0f3d3666ca54e727d4f3f5905
Andrew Moon <andrewm@grep.net.au>
2014-06-18 15:16:23 +1000

    Added install script for 3.3.0

---
+++ www/magento/app/code/community/Signifyd/Connect/sql/signifyd_connect_setup/mysql4-install-3.3.0.php
@@ -0,0 +1,19 @@
+<?php
+
+$this->startSetup();
+$this->register();
+$this->run("
+DROP TABLE IF EXISTS `{$this->getTable('signifyd_connect_case')}`;
+CREATE TABLE IF NOT EXISTS `{$this->getTable('signifyd_connect_case')}` (
+  `case_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `order_increment` varchar(255) NOT NULL,
+  `signifyd_status` varchar(64) NOT NULL DEFAULT 'PENDING',
+  `code` varchar(255) NOT NULL,
+  `score` float DEFAULT NULL,
+  `entries` text NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`case_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+");
+$this->endSetup();
