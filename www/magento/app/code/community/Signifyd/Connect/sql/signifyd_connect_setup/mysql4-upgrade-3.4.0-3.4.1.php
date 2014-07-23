da164ee0ce116673b6956ddb264e27de1da33043
Andrew Moon <andrewm@grep.net.au>
2014-07-23 14:48:57 +1000

    Added v3.4.1

---
+++ www/magento/app/code/community/Signifyd/Connect/sql/signifyd_connect_setup/mysql4-upgrade-3.4.0-3.4.1.php
@@ -0,0 +1,10 @@
+<?php
+
+try {
+    $this->startSetup();
+    $this->register();
+    $this->checkColumns();
+    $this->endSetup();
+} catch (Exception $e) {
+    Mage::log('Signifyd_Connect upgrade: ' . $e->__toString(), null, 'signifyd_connect.log');
+}
