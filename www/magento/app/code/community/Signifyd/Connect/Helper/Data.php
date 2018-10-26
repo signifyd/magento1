24ed339631e75a5ce20cf7aa98678afa9ba21fb9
Ébano Penha Andrello Lopes <ebano.lopes@osf-commerce.com>
2018-10-26 10:44:13 -0300

    MAG-290: Minor fix to backwards compatibility Added 'main_table' on filter to old Magento versions compatibility

--- www/magento/app/code/community/Signifyd/Connect/Helper/Data.php
+++ www/magento/app/code/community/Signifyd/Connect/Helper/Data.php
@@ -503,7 +503,7 @@ class Signifyd_Connect_Helper_Data extends Mage_Core_Helper_Abstract
         if (!empty($incrementId)) {
             /** @var Mage_Sales_Model_Mysql4_Order_Grid_Collection $collection */
             $collection = Mage::getResourceModel('sales/order_grid_collection');
-            $collection->addFieldToFilter('increment_id', $incrementId);
+            $collection->addFieldToFilter('main_table.increment_id', $incrementId);
             /** @var Mage_Sales_Model_Order $orderGrid */
             $orderGrid = $collection->getFirstItem();
 
